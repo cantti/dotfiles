e9edece00d4599d3dd534b0e2d308877e2a2d5df
cantti <cantti@outlook.com>
2024-01-29 00:38:48 +0100

    nerdcommenter fix

--- nvim/.config/nvim/lua/plugins/nerdcommenter.lua
+++ nvim/.config/nvim/lua/plugins/nerdcommenter.lua
@@ -2,7 +2,7 @@ return {
   "preservim/nerdcommenter",
   init = function()
     vim.g.NERDCreateDefaultMappings = 0
-    vim.g.NERDSpaceDelims = 4
+    vim.g.NERDSpaceDelims = 1
     vim.g.NERDDefaultAlign = 'left'
     vim.g.NERDToggleCheckAllLines = 1
   end,
